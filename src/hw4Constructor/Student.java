615019414150e49318833e25b780de0053468df9
this-is-emon <mofijul.islam.emon@gmail.com>
2025-10-21 23:17:21 -0400

    Assignment on Java constructor submitted

---
+++ src/hw4Constructor/Student.java
@@ -0,0 +1,31 @@
+/*
+This Assignment is related to: Assignment Java Constructor
+*/
+package hw4Constructor; // Step 1: Package name --> "hw4Constructor"
+
+// Step 2: Class --> "Student", Declaring variables
+public class Student {
+	String name;
+	int id;
+	char gender;
+	float grade;
+	boolean isProgrammer;
+
+	// Step 3: Default constructor.
+	public Student() {
+		System.out.println("This is all about Student.");
+	}
+
+	// Step 4: Declaring parameterized constructor
+	public Student(String name, char gender, int id, float grade, boolean isProgrammer) {
+		this.name = name;
+		this.id = id;
+		this.grade = grade;
+		this.isProgrammer = isProgrammer;
+
+		System.out.println("Student Name:" + name + ", ID:" + id + ", Gender:" + gender + ", Grade:" + grade
+				+ ", and Java Programmer? " + isProgrammer);
+
+	}
+
+}
---
+++ src/hw4Constructor/StudentTest.java
@@ -0,0 +1,17 @@
+/*
+This Assignment is related to: Assignment Java Constructor
+*/
+package hw4Constructor;
+
+// Step 5: Another class --> StudentTest, Declaring 2 objects which initializes constructors.
+public class StudentTest {
+
+	public static void main(String[] args) {
+
+		// Step 6: First outcome
+		Student student1 = new Student();
+		// Step 7: The second outcome
+		Student student3 = new Student("Tony", 'M', 123, 3.5f, true);
+
+	}
+}
